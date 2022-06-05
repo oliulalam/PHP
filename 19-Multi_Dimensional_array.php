bbb140ce027b1b1a3f04e78459d1c397f760b748
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-06-06 02:14:48 +0600

    Multi-Dimensional Arrays in Php

--- 19-Multi_Dimensional_array.php
+++ 19-Multi_Dimensional_array.php
@@ -1,4 +1,23 @@
 <?php
+$multDim = array(array(3,5,6,7),
+           array(6,4,3,2),
+           array(4,2,5,6));
+
+        //    printing 2 dimensional array
+        for ($i=0; $i<count($multDim); $i++) { 
+            echo var_dump($multDim[$i]);
+            echo "<br>";
+        }
+        echo "<br>";
+
+        for ($i=0; $i<count($multDim); $i++) { 
+            for ($j=0; $j<count($multDim[$i]); $j++) { 
+                echo $multDim[$i][$j];
+                echo " ";
+            }
+            
+            echo "<br>";
+        }
 
 
 ?>
